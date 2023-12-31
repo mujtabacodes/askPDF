c937ceef905189866e811d25f8ec373f1ccf976c
Mujtaba Shafique <mujiroy01@gmail.com>
2023-12-31 22:40:56 +0500

    Working on Upload one file

--- client/src/components/UploadFile/index.tsx
+++ client/src/components/UploadFile/index.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
-import { Container } from './styled'
+import React, { ChangeEvent, useState } from 'react'
+import { Container, Form } from './styled'
+import Button from '@components/Button'
 
 const UploadFile = () => {
-	return <Container>UploadFile</Container>
+	const [file, setFile] = useState(useState<File | null>(null))
+
+	const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+		// Capture the selected file from the input
+		if (event.target.files && event.target.files.length > 0) {
+			setFile(event.target.files[0])
+		}
+	}
+
+	const handleSubmit = () => {
+		// Create an object of formData
+		const formData = new FormData()
+
+		// Update the formData object
+		formData.append('file', file)
+
+		// Request made to the backend api
+		// Send formData object
+		// axios.post('api/uploadfile', formData)
+		// 	.then(res => console.log(res))
+	}
+	return (
+		<Container>
+			<Form onSubmit={handleSubmit}>
+				<input type='file' name='file' onChange={handleFileChange} />
+				<Button>submit</Button>
+			</Form>
+		</Container>
+	)
 }
 
 export default UploadFile
--- client/src/components/UploadFile/styled.ts
+++ client/src/components/UploadFile/styled.ts
@@ -2,4 +2,6 @@ import styled from '@emotion/styled'
 
 export const Container = styled.div`
 	color: black;
+	background: red;
 `
+export const Form = styled.form``

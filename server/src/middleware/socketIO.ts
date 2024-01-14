75edcc616216ca499721f57f674f3ff88d6b5ca1
Mujtaba Shafique <mujiroy01@gmail.com>
2024-01-14 13:46:12 +0500

    Start working on reading document

--- server/src/middleware/socketIO.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from 'express'
-import { Server as SocketIOServer, Socket } from 'socket.io'
-
-export const addIOToRequest = (io: SocketIOServer) => {
-	return (req: Request, res: Response, next: NextFunction) => {
-		req.io = io
-		next()
-	}
-}

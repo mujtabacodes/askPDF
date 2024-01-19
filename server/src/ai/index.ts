2b682446cdf0ad2c3302f49dfd4e2853201b9278
Mujtaba Shafique <mujiroy01@gmail.com>
2024-01-19 18:57:17 +0500

    Working on Langchain

--- server/src/ai/index.ts
+++ server/src/ai/index.ts
@@ -1,27 +1,49 @@
 import { ChatOpenAI } from '@langchain/openai'
-import { HumanMessage } from '@langchain/core/messages'
 import { ChatGPT_API } from '../config'
+import { ChatPromptTemplate } from '@langchain/core/prompts'
+import { createStructuredOutputRunnable } from 'langchain/chains/openai_functions'
+import { JsonOutputFunctionsParser } from 'langchain/output_parsers'
 
-export const ChatGPT = async (prompt: any): Promise<string> => {
+const jsonSchema = {
+	title: 'Person',
+	description: 'Identifying information about a person.',
+	type: 'object',
+	properties: {
+		name: { title: 'Name', description: "The person's name", type: 'string' },
+		age: { title: 'Age', description: "The person's age", type: 'integer' },
+		fav_food: {
+			title: 'Fav Food',
+			description: "The person's favorite food",
+			type: 'string',
+		},
+	},
+	required: ['name', 'age'],
+}
+
+export const ChatGPT = async (prompt: any) => {
 	const chatModel = new ChatOpenAI({
 		temperature: 0.9,
 		openAIApiKey: ChatGPT_API,
 	})
 
 	try {
-		// Send a message to the model
-		const result = await chatModel.invoke([new HumanMessage(prompt)])
-
-		// Check if the result structure is as expected
-		if (result?.AIMessage?.content) {
-			console.log(result)
-			return result.AIMessage.content
-		} else {
-			console.error('Unexpected response format from ChatGPT API:', result)
-			return 'Error: Unexpected response format'
-		}
+		const prompt = ChatPromptTemplate.fromMessages([
+			['human', 'Human description: {description}'],
+		])
+		const outputParser = new JsonOutputFunctionsParser()
+		const runnable = createStructuredOutputRunnable({
+			outputSchema: jsonSchema,
+			llm: model,
+			prompt,
+			outputParser,
+		})
+		const response = await runnable.invoke({
+			description:
+				"My name's John Doe and I'm 30 years old. My favorite kind of food are chocolate chip cookies.",
+		})
+		console.log(response)
 	} catch (error) {
-		console.error('Error communicating with ChatGPT API:', error.message)
+		console.error('Error communicating with ChatGPT API:', `${error}`)
 		throw error
 	}
 }

6c5ba264cf1f5d6bae02e78179db73da8a537d06
Mujtaba Shafique <mujiroy01@gmail.com>
2024-01-30 14:58:09 +0500

    Build

---
+++ server/dist/routes/stripeRoute.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const stripeController_1 = require("../controllers/stripeController");
+const router = (0, express_1.Router)();
+router.post('/', stripeController_1.postCustomer);
+router.get('/status', stripeController_1.checkPaymentStatus);
+// router.post("/", getExampleDataValidation, getExampleData);
+exports.default = router;
